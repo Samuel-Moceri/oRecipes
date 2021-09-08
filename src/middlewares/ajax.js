6ede116964650b2b5f71ecd9d57952a5ab1c930b
samuel.moceri <moceri.samuel@gmail.com>
2021-09-08 10:41:37 +0200

    appel API ok

--- src/middlewares/ajax.js
+++ src/middlewares/ajax.js
@@ -24,6 +24,21 @@ const ajax = (store) => (next) => (action) => {
   }
   else if (action.type === 'LOGIN') {
     console.log('appel de l\'API');
+    // on va vouloir envoyer l'email et le mot de passe à l'api pour s'authentifier
+    // ils sont dans le state, donc je récupère le state puisque j'ai accès au store
+    const state = store.getState();
+    // on fait l'appel à l'api
+    axios.post('http://localhost:3001/login', {
+      email: state.user.email,
+      password: state.user.password,
+    })
+      .then((response) => {
+        console.log('reaction en cas de succès');
+      })
+      .catch((error) => {
+        console.error(error);
+        alert('Authentification échouée');
+      });
   }
   next(action);
 };
